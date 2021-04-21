4bc03edd6b9966a6b132b1900974871bdaf68cbd
hgonomeg <hgonomeg@gmail.com>
2021-04-21 21:55:05 +0200

    Draft GUI

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,20 @@ pub use server::*;
 mod state;
 pub use state::*;
 
+use gtk::prelude::*;
+use gio::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    let application = gtk::Application::new(
+        Some("com.github.hgonomeg.filend"),
+        Default::default(),
+    ).expect("failed to initialize GTK application");
+
+    application.connect_activate(|app| {
+        let window = MainWindow::new();
+        window.set_application(app);
+    });
+
+    let ret = application.run(&std::env::args().collect::<Vec<_>>());
+    std::process::exit(ret);
 }
--- src/main_window.rs
+++ src/main_window.rs
@@ -1,11 +1,19 @@
 use gtk::prelude::*;
 
 pub struct MainWindow {
-    
+    widget: gtk::ApplicationWindow
 }
 
 impl MainWindow {
     pub fn new() -> Self {
-        Self { }
+        let builder = gtk::Builder::from_string(include_str!("main_window.ui"));
+        let widget : gtk::ApplicationWindow = builder.get_object("main_window")
+            .expect("Failed to load the main window");
+        widget.show_all();
+        Self { widget }
+    }
+    pub fn set_application(&self, app: &gtk::Application) {
+        self.widget.set_application(Some(app));
+        app.add_window(&self.widget);
     }
 }
