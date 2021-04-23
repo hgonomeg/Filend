585634f901d51020539854689eccb3777e12ccef
hgonomeg <hgonomeg@gmail.com>
2021-04-23 04:59:42 +0200

    Draft the server

--- src/server.rs
+++ src/server.rs
@@ -1,11 +1,20 @@
 use actix_web::{App,get,post,HttpResponse,HttpServer};
+use std::any::Any;
+use std::boxed::Box;
 
 pub struct Server {
-    
+    server: Box<Any>
 }
 
 impl Server {
     pub fn new() -> Self {
-        Self { }
+        Self {  
+            server: Box::from(
+                HttpServer::new(|| App::new(
+
+                    )
+                )
+            ) 
+        }
     }
 }
