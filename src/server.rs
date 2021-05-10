56c5ca61982bdc3ceba4ceabf58624b17aa97331
hgonomeg <hgonomeg@gmail.com>
2021-05-11 00:16:29 +0200

    Provide a server implementation

--- src/server.rs
+++ src/server.rs
@@ -1,8 +1,12 @@
-use actix_web::{App,get,post,HttpResponse,HttpServer};
+use actix_web::{App,web,get,Result,HttpServer};
+use actix_files::NamedFile;
 use std::thread;
-use std::boxed::Box;
-use std::path::Path;
+use std::collections::HashMap;
+use std::path::{PathBuf,Path};
+use std::sync::{Arc,Mutex};
 use crossbeam_channel::*;
+use futures_executor::block_on;
+use crate::local_files::LocalFiles;
 
 enum ServerCommand {
     Quit
@@ -10,24 +14,49 @@ enum ServerCommand {
 
 pub struct Server {
     server_thread: Option<thread::JoinHandle<()>>,
+    served_files: Arc<Mutex<HashMap<String,PathBuf>>>,
     sender: Sender<ServerCommand>
 }
 
-impl Server {
-    pub fn new(_port: u16) -> Self {
-        let (sender,receiver) = bounded::<ServerCommand>(4);
-        Self {  
-            server_thread: Some(thread::spawn(move || {
-                let _server = HttpServer::new(|| App::new(
+#[derive(Clone)]
+struct ServerAppState {
+    served_files: Arc<Mutex<HashMap<String,PathBuf>>>
+}
 
-                    )
-                );
-            })),
-            sender: sender
+#[get("/filend/{file_id}")] // <- define path parameters
+async fn index(web::Path(file_id): web::Path<String>, data: web::Data<ServerAppState>) ->  Result<NamedFile> {
+    let path: PathBuf = data.served_files.lock().unwrap()[&file_id].clone();
+    Ok(NamedFile::open(path)?)
+}
+
+impl Server {
+    pub fn new(port: u16) -> Self {
+        let (sender,receiver) = bounded::<ServerCommand>(4);
+        let served_files = Arc::from(Mutex::from(HashMap::default()));
+        let state = ServerAppState { served_files: served_files.clone() };
+        let server_handle = Some(thread::spawn(move || {
+            let server = async { 
+                let mut state = Some(state);
+                HttpServer::new(move || {
+                    App::new()
+                        .data(state.clone().take().unwrap())
+                        .service(index)
+                })
+                .bind(format!("127.0.0.1:{}",port).as_str()).unwrap()
+                .run()
+                .await
+            };
+            block_on(server).unwrap();
+        }));
+
+        Self {  
+            server_thread: server_handle,
+            sender: sender,
+            served_files: served_files
         }
     }
     pub fn add_file(&self, id: u32, path: &Path) {
-
+        self.served_files.lock().unwrap().insert(LocalFiles::id_to_hexstring(id),path.to_owned());
     }
 }
 
