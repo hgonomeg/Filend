d15e3aa7e56a02ccb13d4238e6a8c5febafec0f0
hgonomeg <hgonomeg@gmail.com>
2021-05-11 01:19:20 +0200

    Make the server stop

--- src/server.rs
+++ src/server.rs
@@ -31,14 +31,16 @@ async fn index(web::Path(file_id): web::Path<String>, data: web::Data<ServerAppS
 #[actix_rt::main]
 async fn server_main(state: ServerAppState, port: u16) -> std::result::Result<(), std::io::Error> {
     let mut state = Some(state);
-    HttpServer::new(move || {
+    let server = HttpServer::new(move || {
         App::new()
             .data(state.clone().take().unwrap())
             .service(index)
     })
-    .bind(format!("0.0.0.0:{}",port).as_str())?
-    .run()
-    .await
+    .bind(format!("0.0.0.0:{}",port).as_str()).expect(format!("Could not bind to 0.0.0.0:{}",port).as_str())
+    .shutdown_timeout(2)
+    .run();
+    eprintln!("Running!");
+    server.await
 }
 
 impl Server {
@@ -59,11 +61,15 @@ impl Server {
     pub fn add_file(&self, id: u32, path: &Path) {
         self.served_files.lock().unwrap().insert(LocalFiles::id_to_hexstring(id),path.to_owned());
     }
+    pub fn stop() {
+        actix_rt::System::current().stop();
+    }
 }
 
 impl Drop for Server {
     fn drop(&mut self) {
+        Server::stop();
         let server_thread = self.server_thread.take().unwrap();
-        server_thread.join().unwrap();
+        server_thread.join().expect("Could not join the server thread!");
     }
 }
