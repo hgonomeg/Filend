e3578f852127bfd441f396d4fc4edbf990011ee7
hgonomeg <hgonomeg@gmail.com>
2021-07-27 02:14:30 +0200

    Update the 'about window' and extend version info

--- src/about_window.rs
+++ src/about_window.rs
@@ -1,17 +1,27 @@
 use gtk::prelude::*;
 
 pub struct AboutWindow {
-    widget: gtk::Window
+    widget: gtk::Window,
+    version_label: gtk::Label
 }
 
 impl AboutWindow {
+    pub fn init(&mut self) {
+        let ver_str = concat!("version: ",env!("CARGO_PKG_VERSION"));
+        self.version_label.set_text(ver_str);
+    }
     pub fn new() -> Self {
         let builder = gtk::Builder::from_string(include_str!("about_window.ui"));
         let widget : gtk::Window = builder.get_object("about_window")
             .expect("Failed to load the about window");
+        let version_label : gtk::Label = builder.get_object("version_label")
+            .expect("Failed to load the version label");
         widget.show_all();
-        Self{
-            widget
-        }
+        let mut ret = Self{
+            widget,
+            version_label
+        };
+        ret.init();
+        ret
     }
 }
